24a39da389445d705480574577769d45f84208e0
Fixoon <oskar.larsson94@gmail.com>
2017-05-16 22:43:13 +0200

    Updates to expand projects

--- scripts/modal.js
+++ scripts/modal.js
@@ -38,18 +38,15 @@ function popUp(popUpText, projectDiv){
   .css("position", "fixed")
   .css("z-index", "999");
 
-  console.log(clonedDiv.css("width"));
-
-
   //Set text in P to the final text to calculate final height and reset
   var clonedDivP = clonedDiv.find("p");
   clonedDivP.html(newText);
   clonedDiv.css("height", "auto").css("width", "100%");
   var newWidth = clonedDiv.outerWidth();
   var newHight = clonedDiv.outerHeight();
-  if(clonedDiv.height() + 2 < (clonedDivP.height() + clonedDiv.find("h3").height())){
+  /*if(clonedDiv.height() + 2 < (clonedDivP.height() + clonedDiv.find("h3").height())){
     clonedDivP.css("max-height", (clonedDiv.height() - clonedDiv.find("h3").height()));
-  }
+  }*/
   clonedDiv.css("height", height).css("width", width);
   clonedDivP.css("display", "none");
   //Add a background to the modal
@@ -62,12 +59,11 @@ function popUp(popUpText, projectDiv){
   });
   projDiv.css("opacity", "0");
 
-  setTimeout(function(){
-    clonedDiv.css("width");
-    clonedDiv.addClass("test-animation");
-    //clonedDiv.css("transition-property", "left, margin-left, top, margin-top, width, height").css("transition-duration", ".4s");
-    clonedDiv.css("top", "50%").css("left", "50%").css("margin-top", -(newHight/2)).css("margin-left", -(newWidth/2)).css("width", newWidth).css("height", newHight);
-  }, 0);
+
+  clonedDiv.css("width");
+  clonedDiv.addClass("test-animation");
+  clonedDiv.css("top", "50%").css("left", "50%").css("margin-top", -(newHight/2)).css("margin-left", -(newWidth/2)).css("width", newWidth).css("height", newHight);
+
 
   clonedDiv.one("transitionend", function() {
       clonedDivP.css("display", "block");
